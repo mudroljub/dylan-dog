cf67820c4ccc9621f2be6d61d2134a9bca6e0d73
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-10 00:56:38 +0100

    pomalo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var input = document.getElementById('input')
-input.addEventListener('change', function() {
-  readXlsxFile(input.files[0]).then(function(rows) {
-    console.log(rows)
-  })
-})
+import izdanja from './data/redovna.json' assert {type: 'json'}
+
+const zaglavlje = izdanja.shift()
+
+console.log(zaglavlje)
+console.log(izdanja)
