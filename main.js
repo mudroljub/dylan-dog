adcffcd360a6f167399f120e1279760d8db9538f
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-10 18:08:07 +0100

    sve radi

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import svaIzdanja from './data/redovna.json' assert {type: 'json'}
+import izdanja from './data/redovna.json' assert {type: 'json'}
 
-const sakrij = [3, 4, 5, 6, 9, 12, 15, 16, 17, 18]
+const sacuvano = new Set(JSON.parse(localStorage.getItem('sacuvano')))
 
-const svaZaglavlja = [
+const zaglavlje = [
   "br.",      // 0
   "naslov",   // 1
   "god.",     // 2
@@ -23,15 +23,22 @@ const svaZaglavlja = [
   "",         // 17
   "god.",     // 18
 ]
+const sakrij = [3, 4, 5, 6, 9, 12, 15, 16, 17, 18]
 
-const zaglavlje = svaZaglavlja.filter((x, i) => !sakrij.includes(i))
-const izdanja = svaIzdanja.map(izdanje => izdanje.filter((x, i) => !sakrij.includes(i)))
+const zaglavljeHtml = zaglavlje
+  .filter((x, i) => !sakrij.includes(i))
+  .map(th => `<th>${th}</th>`).join('')
 
-const zaglavljeHtml = zaglavlje.map(th => `<th>${th}</th>`).join('')
-
-const redoviHtml = izdanja.map(red => 
-  `<tr><td><input type="checkbox" name="${red[0]}"></td>${red.map(td => `<td>${td || ''}</td>`).join('')}</tr>`
+const redoviHtml = izdanja
+.map(red => red.filter((col, i) => !sakrij.includes(i)))
+.map(red =>
+  `<tr>
+    <td><input type="checkbox" name="${red[0]}" ${sacuvano.has(red[0]) ? 'checked' : ''} ></td>
+    ${red.map(td => `<td>${td || ''}</td>`).join('')}
+  </tr>`
 ).join('')
+
+/* RENDER */
 
 document.body.innerHTML += `
 <table>
@@ -46,3 +53,12 @@ document.body.innerHTML += `
   </tbody>
 </table>
 `
+
+/* EVENTS */
+
+document.addEventListener('click', ({ target }) => {
+  if (!target.name) return
+  if (target.checked) sacuvano.add(Number(target.name))
+  else sacuvano.delete(Number(target.name))
+  localStorage.setItem('sacuvano', JSON.stringify([...sacuvano]))
+})
