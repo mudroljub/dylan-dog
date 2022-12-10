d86437c827742ab0ca60b6c37af22f5ce7efb851
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-10 01:18:56 +0100

    renda

--- main.js
+++ main.js
@@ -2,5 +2,22 @@ import izdanja from './data/redovna.json' assert {type: 'json'}
 
 const zaglavlje = izdanja.shift()
 
-console.log(zaglavlje)
-console.log(izdanja)
+// console.log(zaglavlje)
+// console.log(izdanja)
+
+const zaglavljeHtml = zaglavlje.map(th => `<th>${th}</th>`).join('')
+
+const izdanjaHtml = izdanja.map(red => {
+  const redHtml = red.map(td => `<td>${td}</td>`).join('')
+  return `<tr>${redHtml}</tr>`
+}).join('')
+
+
+document.body.innerHTML = `
+<table>
+  <tr>
+    ${zaglavljeHtml}
+  </tr>
+  ${izdanjaHtml}
+</table>
+`
