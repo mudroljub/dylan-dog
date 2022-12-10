1720963eccb72aeeee9b70c4355119e1ce74fb1b
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-10 12:35:05 +0100

    hide columns

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 import svaIzdanja from './data/redovna.json' assert {type: 'json'}
 
-const sakrij = [3, 5, 6, 16, 17, 18]
+const sakrij = [3, 5, 6, 9, 12, 15, 16, 17, 18]
 
 const svaZaglavlja = [
-  "",
-  "br.",
-  "naziv",
-  "god.",
-  "str",
-  "scenario",
-  "crtež",
-  "naslovna",
-  "Dnevnik<small> / Abaton / ostali</small>",
-  "prevod",
-  "god.",
-  "Ludens<small> / Slobodna dalmacija</small>",
-  "prevod",
-  "god.",
-  "Veseli četvrtak",
-  "prevod",
-  "god.",
-  "Libellus",
-  "prevod",
-  "god.",
+  "br.",    // 0
+  "italijanski naslov",  // 1
+  "god.",   // 2
+  "str",    // 3
+  "scenario", // 4
+  "crtež",    // 5
+  "naslovna", // 6
+  "Dnevnik<small> / Abaton / ostali</small>", // 7
+  "naslov", // 8
+  "god.",   // 9 
+  "Ludens<small> / Slobodna dalmacija</small>", // 10
+  "naslov", // 11
+  "god.",   // 12
+  "Veseli četvrtak", // 13
+  "naslov",   // 14
+  "god.",     // 15
+  "Libellus", // 16
+  "naslov",   // 17
+  "god.",     // 18
 ]
 
 const zaglavlje = svaZaglavlja.filter((x, i) => !sakrij.includes(i))
@@ -36,9 +35,14 @@ const redoviHtml = izdanja.map((red, i) =>
 
 document.body.innerHTML = `
 <table>
-  <tr>
-    ${zaglavljeHtml}
-  </tr>
-  ${redoviHtml}
+  <thead>
+    <tr>
+      <th></th>
+      ${zaglavljeHtml}
+    </tr>
+  </thead>
+  <tbody>
+    ${redoviHtml}
+  </tbody>
 </table>
 `
