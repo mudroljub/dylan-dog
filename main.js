ff45501a310a2562b67526c1b53cc491794a7c94
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-10 19:01:22 +0100

    vraćeni podaci

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ import izdanja from './data/redovna.json' assert {type: 'json'}
 
 const sacuvano = new Set(JSON.parse(localStorage.getItem('sacuvano')))
 
-const zaglavlje = [
+const zaglavlje = 
+[
   "br.",      // 0
   "naslov",   // 1
   "god.",     // 2
@@ -10,20 +11,29 @@ const zaglavlje = [
   "scenario", // 4
   "crtež",    // 5
   "naslovna", // 6
-  "Dnevnik<small> / ostali</small>", // 7
-  "",         // 8
-  "god.",     // 9 
-  "Ludens<small> / Slobodna<br>dalmacija</small>", // 10
-  "",         // 11
-  "god.",     // 12
-  "Veseli četvrtak", // 13
-  "",         // 14
+  "boja",     // 7
+  "edicija",  // 8
+  "br.",      // 9
+  "naslov",   // 10
+  "god.",     // 11
+  "edicija",  // 12
+  "br.",      // 13
+  "naslov",   // 14
   "god.",     // 15
-  "Libellus", // 16
-  "",         // 17
-  "god.",     // 18
+  "edicija",  // 16
+  "br.",      // 17
+  "naslov",   // 18
+  "god.",     // 19
+  "edicija",  // 20
+  "br.",      // 21
+  "naslov",   // 22
+  "god.",     // 23
+  "edicija",  // 24
+  "br.",      // 25
+  "naslov",   // 26
+  "god."      // 27
 ]
-const sakrij = [3, 4, 5, 6, 9, 12, 15, 16, 17, 18]
+const sakrij = [3, 5, 6, 7, 11, 15, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
 
 const zaglavljeHtml = zaglavlje
   .filter((x, i) => !sakrij.includes(i))
