ec2a9b57f02a5fc6347a5573e90a9e9b4615418e
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-10 17:05:48 +0100

    m

--- main.js
+++ main.js
@@ -3,24 +3,24 @@ import svaIzdanja from './data/redovna.json' assert {type: 'json'}
 const sakrij = [3, 4, 5, 6, 9, 12, 15, 16, 17, 18]
 
 const svaZaglavlja = [
-  "br.",    // 0
-  "naslov",  // 1
-  "god.",   // 2
-  "str",    // 3
+  "br.",      // 0
+  "naslov",   // 1
+  "god.",     // 2
+  "str",      // 3
   "scenario", // 4
   "crtež",    // 5
   "naslovna", // 6
   "Dnevnik<small> / ostali</small>", // 7
-  "", // 8
-  "god.",   // 9 
+  "",         // 8
+  "god.",     // 9 
   "Ludens<small> / Slobodna<br>dalmacija</small>", // 10
-  "", // 11
-  "god.",   // 12
+  "",         // 11
+  "god.",     // 12
   "Veseli četvrtak", // 13
-  "",   // 14
+  "",         // 14
   "god.",     // 15
   "Libellus", // 16
-  "",   // 17
+  "",         // 17
   "god.",     // 18
 ]
 
