c965e61a438ea9901b6ab28c73cac7c2d44e6ebf
Damjan Pavlica <damjan.pavlica@origo.is>
2022-12-10 16:54:41 +0100

    logo

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 import svaIzdanja from './data/redovna.json' assert {type: 'json'}
 
-const sakrij = [3, 5, 6, 9, 12, 15, 16, 17, 18]
+const sakrij = [3, 4, 5, 6, 9, 12, 15, 16, 17, 18]
 
 const svaZaglavlja = [
   "br.",    // 0
-  "italijanski naslov",  // 1
+  "naslov",  // 1
   "god.",   // 2
   "str",    // 3
   "scenario", // 4
   "crtež",    // 5
   "naslovna", // 6
   "Dnevnik<small> / ostali</small>", // 7
-  "naslov", // 8
+  "", // 8
   "god.",   // 9 
   "Ludens<small> / Slobodna<br>dalmacija</small>", // 10
-  "naslov", // 11
+  "", // 11
   "god.",   // 12
   "Veseli četvrtak", // 13
-  "naslov",   // 14
+  "",   // 14
   "god.",     // 15
   "Libellus", // 16
-  "naslov",   // 17
+  "",   // 17
   "god.",     // 18
 ]
 
@@ -29,13 +29,11 @@ const izdanja = svaIzdanja.map(izdanje => izdanje.filter((x, i) => !sakrij.inclu
 
 const zaglavljeHtml = zaglavlje.map(th => `<th>${th}</th>`).join('')
 
-const renderRed = (td, i) => `<td>${(i == 1 ? `<i>${td}</i>` : td) || ''}</td>`
-
-const redoviHtml = izdanja.map((red, i) => 
-  `<tr><td><input type="checkbox" name="${i}"></td>${red.map(renderRed).join('')}</tr>`
+const redoviHtml = izdanja.map(red => 
+  `<tr><td><input type="checkbox" name="${red[0]}"></td>${red.map(td => `<td>${td || ''}</td>`).join('')}</tr>`
 ).join('')
 
-document.body.innerHTML = `
+document.body.innerHTML += `
 <table>
   <thead>
     <tr>
